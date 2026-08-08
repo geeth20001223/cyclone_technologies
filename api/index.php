c24fc6f35c0f7d8919461cc14fd7ca07a04da885
geeth2000 <shamal.geethanjanpathirana@gmail.com>
2026-08-09 01:44:32 +0530

    fix: redirect Laravel bootstrap caches to /tmp and set vercel-php@0.6.0

--- api/index.php
+++ api/index.php
@@ -20,10 +20,27 @@ foreach ($storageDirs as $dir) {
     }
 }
 
+// Redirect all Laravel cache & bootstrap files to writable /tmp
 putenv('VERCEL=1');
 $_ENV['VERCEL'] = '1';
+
 putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
+putenv('APP_SERVICES_CACHE=/tmp/storage/bootstrap/cache/services.php');
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/storage/bootstrap/cache/services.php';
+
+putenv('APP_PACKAGES_CACHE=/tmp/storage/bootstrap/cache/packages.php');
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/storage/bootstrap/cache/packages.php';
+
+putenv('APP_CONFIG_CACHE=/tmp/storage/bootstrap/cache/config.php');
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/storage/bootstrap/cache/config.php';
+
+putenv('APP_ROUTES_CACHE=/tmp/storage/bootstrap/cache/routes.php');
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/storage/bootstrap/cache/routes.php';
+
+putenv('APP_EVENTS_CACHE=/tmp/storage/bootstrap/cache/events.php');
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/storage/bootstrap/cache/events.php';
 
 define('LARAVEL_START', microtime(true));
 
